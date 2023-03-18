ce189d3c7cceefb43727b41184cfea1ad4afb32e
Fabio <fabiodesenvolvedorpro@gmail.com>
2023-03-18 18:04:02 -0300

    adicionado layout a pagina

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>BurgerTime</title>
+</head>
+<body>
+    <header>
+        <!-- Cabeçalho do site -->
+    </header>
+
+    <nav>
+    NAV
+    </nav>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        FOOTER
+    </footer>
+</body>
+</html>
